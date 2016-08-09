07ae5355f35c9747a7db6800e80e34a02f65110e
Kris Shaffer <kris.shaffer@gmail.com>
2016-08-08 21:15:53 -0400

    lots of comments added

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// The lists of most common (American) English words for several parts of speech.
+// These are JavaScript arrays.
+
 var nouns = ['time', 'person', 'year', 'way', 'day', 'thing', 'man', 'world', 'life', 'hand', 'part', 'child', 'eye', 'woman', 'place', 'work', 'week', 'case', 'point', 'government', 'company', 'number', 'group', 'problem', 'fact'];
 
 var plural_nouns = ['times', 'persons', 'years', 'ways', 'days', 'things', 'men', 'worlds', 'lives', 'hands', 'parts', 'children', 'eyes', 'women', 'places', 'works', 'weeks', 'cases', 'points', 'governments', 'companies', 'numbers', 'groups', 'problems', 'facts'];
@@ -8,25 +11,42 @@ var prepositions = ['to', 'of', 'in', 'for', 'on', 'with', 'at', 'by', 'from', '
 
 var articles = ['a', 'an', 'the'];
 
+// Some Pride-and-Prejudice-specific words that will make the replacements more interesting.
+
 var surnames_pnp = ['Bennet', 'Bingley', 'Netherfield', 'Long']
 
+// The first couple pages of Pride and Prejudice.
+
 var original_text = '<p> It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. </p>\n<p> However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families that he is considered as the rightful property of some one or other of their daughters. </p>\n<p> "My dear Mr. Bennet," said his lady to him one day, "have you heard that Netherfield Park is let at last?" </p>\n<p> Mr. Bennet replied that he had not. </p>\n<p> "But it is," returned she; "for Mrs. Long has just been here, and she told me all about it." </p>\n<p> Mr. Bennet made no answer. </p>\n<p> "Do not you want to know who has taken it?" cried his wife impatiently. </p>\n<p> "You want to tell me, and I have no objection to hearing it." </p>\n<p> This was invitation enough. </p>\n<p> "Why, my dear, you must know, Mrs. Long says that Netherfield is taken by a young man of large fortune from the north of England; that he came down on Monday in a chaise and four to see the place, and was so much delighted with it, that he agreed with Mr. Morris immediately; that he is to take possession before Michaelmas, and some of his servants are to be in the house by the end of next week." </p>\n<p> "What is his name?" </p>\n<p> "Bingley." </p>\n<p> "Is he married or single?" </p>\n<p> "Oh! single, my dear, to be sure! A single man of large fortune; four or five thousand a year. What a fine thing for our girls!" </p>\n<p> "How so? how can it affect them?" </p>';
+
+// This function puts the P&P text into the website.
 
 function insertText () {
   document.getElementById('shakespeare').innerHTML = original_text;
 }
 
+// This variable makes the replace_random_word() function repeat by default.
+
 var keep_going = true;
+
+// The main function, which finds a random word in the text and if it finds it in one of the lists above,
+// replaces it with a different word from that list.
 
 function replace_random_word() {
 
-  // get text and split it by spaces
+  // Get text from the website and split it by word into a list (array), using spaces to delimit words.
+
   var the_text = document.getElementById('shakespeare').innerHTML;
   var the_text_words = the_text.split(' ');
 
-  // pick random word in array of words
+  // Pick a random word in the array of words.
+
   var random_index = Math.floor(Math.random() * the_text_words.length);
   var random_word = the_text_words[random_index];
+
+  // Find the punctuation-free word without erasing the random_word variable that contains the punctuation
+  // (which we'll need later).
+
   var random_word_no_punc = random_word.replace(',', '').replace('.', '').replace('?', '').replace('!', '').replace(';', '');
   var random_word_no_punc_no_quote = random_word_no_punc.replace(/"/g, '');
   if (random_word[0] == '"') {
@@ -42,8 +62,10 @@ function replace_random_word() {
   console.log(random_word, random_word_no_punc, random_word_no_punc_no_quote);
 
   // WORD REPLACEMENT LOOP
-  // search word lists for target word
-  // if present in a list, replace it with another word in that list
+
+  // Search part-of-speech lists for the selected target word.
+  // If present in one of those lists, replace it with another word in that list.
+
   if (nouns.includes(random_word_no_punc_no_quote)) {
     new_word = nouns[Math.floor(Math.random() * nouns.length)];
   } else if (plural_nouns.includes(random_word_no_punc_no_quote)) {
@@ -60,8 +82,8 @@ function replace_random_word() {
     new_word = random_word_no_punc_no_quote;
   }
 
+  // If there was punctuation in the originally selected word, add it to the new word.
 
-  // deal with punctuation
   if (random_word != random_word_no_punc && trailing_quote == false) {
     new_word += random_word[random_word.length-1];
   } else if (random_word != random_word_no_punc && trailing_quote == true) {
@@ -73,18 +95,31 @@ function replace_random_word() {
   if (leading_quote == true) {
     new_word = '"' + new_word;
   }
+
+  // Log the new word to the developer console for debugging purposes.
+
   console.log(new_word);
+
+  // Put the new word (with punctuation, if necessary) into the word array.
+
   the_text_words[random_index] = new_word;
 
-  // reconstruct text
+  // Use the word array to Reconstruct a website-friendly text block.
+
   var the_new_text = the_text_words.join(' ');
 
-  // replace text on page
+  // Replace the whole text block on the web page with the new text block (without reloading the page).
+
   document.getElementById('shakespeare').innerHTML = the_new_text;
+
+  // Repeat the proess until keep_going = false.
+
   if (keep_going == true) {
     setTimeout('replace_random_word()', 250);
   }
 }
+
+// A function to stop the replace_random_word() function from looping.
 
 function stop () {
   keep_going = false;
